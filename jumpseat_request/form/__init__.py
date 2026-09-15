82da5251d4347ebf7c8c31cdd0d93c59206ca226
Carl Harris <hitbox@ryzen.com>
2026-09-15 15:16:11 -0400

    __init__ for html help lib

--- jumpseat_request/form/__init__.py
+++ jumpseat_request/form/__init__.py
@@ -2,7 +2,6 @@ from .airline import EditAirlineForm
 from .airline import NewAirlineForm
 from .announcement import EditAnnouncementForm
 from .announcement import NewAnnouncementForm
-from .application_setting import EditApplicationSettingForm
 from .employee import EditEmployeeForm
 from .employee import EmployeeSubForm
 from .employee import NewEmployeeForm
